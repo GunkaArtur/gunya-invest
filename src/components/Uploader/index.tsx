93e53a4e72d0f12959930fc71447a0c59461628d
Artur Gunca <arturel101010@mail.ru>
2024-02-17 11:58:15 +0200

    fix bug with tax length

--- src/components/Uploader/index.tsx
+++ src/components/Uploader/index.tsx
@@ -67,35 +67,62 @@ export const Uploader = ({ onUpload }: Props) => {
           .slice(1)
           .slice(0, -1);
 
-        if (taxes.length === dividends.length) {
-          const parsedArr = dividends.map((item, idx): ItemWithCalcTax => {
-            const exchange = exchangeRates.find((it) => it.date === item.date);
+        const newOne = dividends.map((div) => {
+          const tax = taxes.find(
+            (tx) => tx.date === div.date && tx.company === div.company,
+          );
+          const exchange = exchangeRates.find((it) => it.date === div.date);
 
-            return {
-              ...item,
-              tax: taxes[idx].amount ?? 0,
-              netProfit: item.amount - taxes[idx].amount * -1,
-              mdlAmount: exchange ? item.amount * exchange.usd : -1,
-              taxMDL: exchange ? taxes[idx].amount * exchange.usd * -1 : -1,
-            };
-          });
-
-          dispatch(addDividend(parsedArr));
-          onUpload();
-        } else {
-          const lastD = dividends.map((div) => {
-            const exchange = exchangeRates.find((it) => it.date === div.date);
+          if (tax) {
             return {
               ...div,
-              netProfit: div.amount,
+              tax: tax.amount,
+              netProfit: div.amount - tax.amount * -1,
               mdlAmount: exchange ? div.amount * exchange.usd : -1,
-              taxMDL: 0,
+              taxMDL: exchange ? tax.amount * exchange.usd * -1 : -1,
             };
-          });
+          }
+          return {
+            ...div,
+            netProfit: div.amount,
+            mdlAmount: exchange ? div.amount * exchange.usd : -1,
+            taxMDL: 0,
+          };
+        });
+        // console.log("HERE", newOne);
 
-          dispatch(addDividend(lastD));
-          onUpload();
-        }
+        dispatch(addDividend(newOne));
+        onUpload();
+
+        // if (taxes.length === dividends.length) {
+        //   const parsedArr = dividends.map((item, idx): ItemWithCalcTax => {
+        //     const exchange = exchangeRates.find((it) => it.date === item.date);
+        //
+        //     return {
+        //       ...item,
+        //       tax: taxes[idx].amount ?? 0,
+        //       netProfit: item.amount - taxes[idx].amount * -1,
+        //       mdlAmount: exchange ? item.amount * exchange.usd : -1,
+        //       taxMDL: exchange ? taxes[idx].amount * exchange.usd * -1 : -1,
+        //     };
+        //   });
+        //
+        //   dispatch(addDividend(parsedArr));
+        //   onUpload();
+        // } else {
+        //   const lastD = dividends.map((div) => {
+        //     const exchange = exchangeRates.find((it) => it.date === div.date);
+        //     return {
+        //       ...div,
+        //       netProfit: div.amount,
+        //       mdlAmount: exchange ? div.amount * exchange.usd : -1,
+        //       taxMDL: 0,
+        //     };
+        //   });
+        //
+        //   dispatch(addDividend(lastD));
+        //   onUpload();
+        // }
       },
     });
   };
